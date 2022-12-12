d1e41f2b169c2a987408d8c73ed4af2411bbab64
virtyoszt <koidanzero@gmail.com>
2022-12-12 14:10:27 +0200

    5 lab

---
+++ lab5/LR_5_task_8.py
@@ -0,0 +1,25 @@
+import numpy as np
+import neurolab as nl
+import numpy.random as rand
+import pylab as pl
+
+skv = 0.04
+centr = np.array([[0.1, 0.2], [0.4, 0.3], [0.7, 0.3], [0.2, 0.5], [0.5, 0.3]])
+rand_norm = skv * rand.randn(100, 5, 2)
+inp = np.array([centr + r for r in rand_norm])
+inp.shape = (100 * 5, 2)
+rand.shuffle(inp)
+net = nl.net.newc([[0.0, 1.0],[0.0, 1.0]], 5)
+error = net.train(inp, epochs=200,  show=20)
+pl.title('Classification Problem')
+pl.subplot(211)
+pl.plot(error)
+pl.xlabel('Epoch number')
+pl.ylabel('error (default MAE)')
+w = net.layers[0].np['w']
+pl.subplot(212)
+pl.plot(inp[:,0], inp[:,1], '.',
+        centr[:,0], centr[:, 1], 'yv',
+        w[:,0], w[:,1], 'p')
+pl.legend(['train samples', 'real centers', 'train centers'])
+pl.show()
